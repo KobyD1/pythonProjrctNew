c2d0bcc9b7d42d1ffb3da37ccbd728b38a006d34
dkdk1 <dkdk100@walla.co.il>
2023-11-18 17:57:42 +0200

    first commit for booking

---
+++ Selenium_fixtures/Pages/WelcomeBookingPage.py
@@ -0,0 +1,26 @@
+import time
+
+from selenium.common import WebDriverException
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+
+from noTraffic.Pages.Locators import WelcomePageLocators
+
+
+class welcomeBookingPage(object):
+
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.search_menu = self.driver.find_element(By.ID,":re:")
+
+
+
+    def click_on_search_menu(self,text):
+        self.search_menu.click()
+        self.search_menu.send_keys(text)
+
+    def close_pop_up(self):
+        time.sleep(3)
+        close_button = self.driver.find_elements(By.CSS_SELECTOR, "div[class*='abcc616ec7']")[1]
+        close_button.click()
---
+++ Selenium_fixtures/Tests/BaseSelenium.py
@@ -0,0 +1,9 @@
+
+class baseSelenium():
+    url = 'http://www.booking.com'
+
+
+
+
+
+
