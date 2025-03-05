a38a056e05b28774105cd8fa7e8d8577ff69961e
dkdk1 <dkdk100@walla.co.il>
2025-03-05 15:09:02 +0200

    fixing dogs name

--- tipalti/dev_dogs/tests/dev_dogs_test.py
+++ tipalti/dev_dogs/tests/dev_dogs_test.py
@@ -20,14 +20,14 @@ class TestDevDogs():
 
     def test_filling_content_lychee(self, setup_dev_dogs):
         page, welcome_page, dog_page = setup_dev_dogs
-        welcome_page.click_on_item_and_verify(dogsNames.Kika.value)
+        welcome_page.click_on_item_and_verify(dogsNames.Lychee.value)
         dog_page.fill_contant_details(CONTACT_NAME, CONTACT_EMAIL)
         dog_page.fill_message(MESSAGE_PREFIX, dogsNames.Lychee.value)
         dog_page.click_on_send()
 
-    def test_filling_content_kymba(self, setup_dev_dogs):
+    def test_filling_content_kimba(self, setup_dev_dogs):
         page, welcome_page, dog_page = setup_dev_dogs
-        welcome_page.click_on_item_and_verify(dogsNames.Kika.value)
+        welcome_page.click_on_item_and_verify(dogsNames.Kimba.value)
         dog_page.fill_contant_details(CONTACT_NAME, CONTACT_EMAIL)
         dog_page.fill_message(MESSAGE_PREFIX, dogsNames.Kimba.value)
         dog_page.click_on_send()
