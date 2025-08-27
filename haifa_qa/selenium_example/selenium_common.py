d38acf04aa9f8fa24f734d0b734ea3aeb82cfae0
dkdk1 <dkdk100@walla.co.il>
2025-08-27 15:24:00 +0300

    adding selenium for haifa_qa

---
+++ haifa_qa/selenium_example/selenium_common.py
@@ -0,0 +1,20 @@
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium import webdriver
+
+class seleniumCommon():
+
+    def selenium_start(self,url):
+        service = ChromeService(executable_path=ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service)
+        self.driver = driver
+        driver.maximize_window()
+        driver.get(url)
+        return driver
+
+    def selenium_end (self):
+        self.driver.close()
+
+        print ("test End")
