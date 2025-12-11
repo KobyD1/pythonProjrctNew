6a519d904f31967e68c973db2b5799b97cf4ad8f
Koby <dkdk100@walla.co.il>
2025-12-11 14:23:55 +0200

    adding new tests

--- kerem_qa/selenium_example/pytest_swaglabs.py
+++ kerem_qa/selenium_example/pytest_swaglabs.py
@@ -1,6 +1,8 @@
 import time
 import unittest
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
 from kerem_qa.selenium_example.seleniumBaseDalya import seleniumBaseDalya
 
 
@@ -24,3 +26,20 @@ class pytest_swaglabs(unittest.TestCase):
         time.sleep(3)   # example of delay
         url = self.driver.current_url
         assert url == "https://www.saucedemo.com/inventory.html","URL did not chnged after login success "
+
+
+    def test_drop_down_exampe(self):
+        print("Into test login")
+        user = self.driver.find_element(By.ID, "user-name")
+        password = self.driver.find_element(By.NAME, "password")
+        login_button = self.driver.find_element(By.ID, "login-button")
+
+        user.send_keys("standard_user")
+        password.send_keys("secret_sauce")
+        login_button.click()
+        sort = self.driver.find_element(By.CLASS_NAME, "product_sort_container")
+        sort_as_drop_down = Select(sort)
+        # sort_as_drop_down.select_by_visible_text("Price (low to high)")
+        sort_as_drop_down.select_by_index(3)
+        print ("ffgf")
+
