564864d70b3140f8a369282a878bfdaf70e74f85
Koby <dkdk100@walla.co.il>
2025-12-11 09:43:26 +0200

    more examples

--- kerem_qa/pytest_example/my_first_pytest.py
+++ kerem_qa/pytest_example/my_first_pytest.py
@@ -5,14 +5,27 @@ from Examples.PythonCharm.Python.PythonTraining.IntegerTest import result
 
 class myFirstPytest(unittest.TestCase):
 
+    def setUp(self):
+        print ("into setup")
+        self.num1= 2
+        self.num2=3
+
+    def tearDown(self):
+        print ("into test end ")
+
+
     def test_summery(self):
-        num1 = 3
-        num2 = 9
-        summery = num1 + num2
+
+        summery = self.num1 + self.num2
 
 
         assert summery==12,"our code did not suport summery "
         result=summery*2
         print (result)
+
+    def test_multiple(self):
+
+        multiple = self.num1 * self.num2
+        assert multiple==28,"our code did not suport multiple "
 
 
