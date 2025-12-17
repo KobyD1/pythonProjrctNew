e11fe742ee4f5881a1e9f3d239e34a6f89631cd2
dkdk1 <dkdk100@walla.co.il>
2025-12-17 14:02:54 +0200

    pom for selenium example

---
+++ kerem_qa/page_object_example/pages/login_page.py
@@ -0,0 +1,18 @@
+
+
+
+
+class loginPage():
+
+
+    def __init__(self,driver):
+        self.driver = driver
+
+
+    def click_on_login_button(self):
+        print ("click_on_login_button")
+        pass
+
+    def set_user_and_password(self):
+
+        pass
---
+++ kerem_qa/page_object_example/tests/sanity_test.py
@@ -0,0 +1,21 @@
+import unittest
+
+from kerem_qa.page_object_example.pages.login_page import loginPage
+from kerem_qa.page_object_example.tests.selenium_base_example import seleniumBaseExample
+
+
+class sanityTest(unittest.TestCase):
+
+
+    def setUp(self):
+        self.base =  seleniumBaseExample()
+        self.driver = self.base.selenium_start_with_url("https://www.saucedemo.com/")
+        self.login_page = loginPage(self.driver)
+
+    def tearDown(self):
+        self.base.selenium_stop()
+
+
+    def test_login_with_correct_parameeters(self):
+        print ("into login with correct parameters test")
+        self.login_page.click_on_login_button()
