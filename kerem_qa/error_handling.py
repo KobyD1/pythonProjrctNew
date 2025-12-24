24e34a8dfe88f056022e9fbf4a44c3dbd778e896
dkdk1 <dkdk100@walla.co.il>
2025-12-24 08:51:02 +0200

    error handling

---
+++ kerem_qa/error_handling.py
@@ -0,0 +1,24 @@
+# block to sample
+try :
+    a = 10
+    b = 5
+    num1 = 20
+    num2 = a -b
+    summery = num1/num2
+
+    # what I looking for
+
+except ZeroDivisionError:
+    print ("Division by zero event found ")
+    num2 =1
+    summery = num1/num2
+# will be done anyway
+finally:
+    summery = summery +5
+
+
+
+
+
+print (summery)
+
