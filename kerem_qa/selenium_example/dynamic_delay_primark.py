e268314618c2968e518e31b0f8a8360d8cbb7cdb
dkdk1 <dkdk100@walla.co.il>
2025-12-31 11:26:32 +0200

    add delay

---
+++ kerem_qa/selenium_example/dynamic_delay_primark.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from kerem_qa.selenium_example.seleniumBaseDalya import seleniumBaseDalya
+
+base = seleniumBaseDalya()
+
+driver = base.selenium_start_with_url("https://www.primark.com/en-gb")
+sale_button = WebDriverWait(driver, 20).until( EC.((By.LINK_TEXT, "SALE")))
+sale_text=sale_button.text
+
+sale_button.click()
+print (F"sale_text: {sale_text}")
+
+
+
+
+
+base.selenium_stop()
