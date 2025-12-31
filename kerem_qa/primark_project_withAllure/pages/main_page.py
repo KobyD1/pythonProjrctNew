62fd8b9ccfe2cc58502ea26fb96d4679de1a5b80
dkdk1 <dkdk100@walla.co.il>
2025-12-31 12:00:37 +0200

    add delay to page object

--- kerem_qa/primark_project_withAllure/pages/main_page.py
+++ kerem_qa/primark_project_withAllure/pages/main_page.py
@@ -1,16 +1,25 @@
 import allure
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from kerem_qa.selenium_example.seleniumBaseDalya import seleniumBaseDalya
 from kerem_qa.primark_project_withAllure.pages.locetors import MainPageLocators
+from kerem_qa.primark_project_withAllure.pages.locetors import CartPageLocetors
+
 
 
 class MainPage:
     def __init__(self,driver):
         self.driver = driver
+        self.wait = WebDriverWait(self.driver, 20)
 
 
     def serach_for_item(self,item="Shoes"):
         self.driver.find_element(*MainPageLocators.SEARCH_FIELD).click()
+        sale_button = self.wait.until(EC.visibility_of_element_located(*MainPageLocators.SEARCH_FIELD))
+
         search = self.driver.find_element(By.ID,"search-field").click()
 
         search.clear()
