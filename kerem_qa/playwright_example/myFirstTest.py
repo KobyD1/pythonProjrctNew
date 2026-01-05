351ee0930ffbc1a5eb7ba8f29ff3dfe2369e4b56
dkdk1 <dkdk100@walla.co.il>
2026-01-05 09:19:14 +0200

    add first commit for playwright

---
+++ kerem_qa/playwright_example/myFirstTest.py
@@ -0,0 +1,12 @@
+from playwright.sync_api import sync_playwright, expect
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("http://www.ebay.com")
+    search = page.locator('#gh-ac')
+    search.click()
+    search.clear()
+    search.fill("Phone")
+    assert page.title() == "Electronics, Cars, Fashion, Collectibles, Coupons and More  | eBay"
+
+    browser.close()
