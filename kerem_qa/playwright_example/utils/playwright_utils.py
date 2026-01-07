993c8bd825e505f8fe8e4d1305922afad972fd42
dkdk1 <dkdk100@walla.co.il>
2026-01-07 14:27:34 +0200

    add fixture

---
+++ kerem_qa/playwright_example/utils/playwright_utils.py
@@ -0,0 +1,23 @@
+
+
+
+class PlaywrightUtils:
+
+    def is_button_clickable(self,element):
+        try:
+            element.click()
+            return True
+
+
+        except Exception as e:
+            print(e)
+            return False
+
+    def is_button_exists(self,element):
+        try:
+            element.click()
+            return True
+        except Exception as e:
+            print(e)
+            print ("element not found ")
+            return False
