2cc57c2a9ed4d2dc77c7383c6959c56275cead8c
jbt <dkdk100@walla.co.il>
2026-01-18 11:24:46 +0200

    adding folders

---
+++ jb/62_training/pom_example/pages/welcome_page.py
@@ -0,0 +1,10 @@
+
+
+
+class WelcomePage:
+    def __init__(self,page):
+        self.page = page
+
+    def set_user_and_password(self,user_text,user_password):
+        self.user_text = user_text
+        self.user_password = user_password
