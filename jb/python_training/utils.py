b28b97303d3139b4a9adcad2e5224ed6c465f36e
jbt <dkdk100@walla.co.il>
2026-01-18 18:52:19 +0200

    prices exac

---
+++ jb/python_training/utils.py
@@ -0,0 +1,15 @@
+
+class Utils_jb61:
+
+    def age_calculator(age,ref_age=18):
+        if age>ref_age:
+            print(f"age is greater than {ref_age}")
+            age_new =age+5
+        else :
+            print(f"age is less than {ref_age}")
+            age_new =age-5
+
+        return age_new
+
+    def email_validator(email):
+        print("trying to analyze if email is valid ")
