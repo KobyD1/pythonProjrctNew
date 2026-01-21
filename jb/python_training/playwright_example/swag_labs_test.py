ad470d5af3bb9162761b283142b4a0d702970d19
jbt <dkdk100@walla.co.il>
2026-01-21 18:57:41 +0200

    add test for swag labs

---
+++ jb/python_training/playwright_example/swag_labs_test.py
@@ -0,0 +1,26 @@
+from playwright.sync_api import sync_playwright
+
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://www.saucedemo.com/")
+    user = page.locator("[id='user-name']")
+    user.fill("standard_user")
+
+
+    login_button = user.locator("[name='login-button']")
+    login_button.click()
+
+
+
+
+
+
+
+
+
+
+    page.close()
+    browser.close()
+    print ("Test end****")
