a80cceec70171b5d3c61c8269e671a82cd8cba12
jbt <dkdk100@walla.co.il>
2026-01-25 18:37:57 +0200

    first pytest

--- jb/python_training/playwright_example/swaglabs_all_test.py
+++ jb/python_training/playwright_example/swaglabs_all_test.py
@@ -2,8 +2,9 @@
 
 class TestSwagLabsTest():
 
-    def test_login(self):
+    def test_login(self,setup_playwright):
         print("test_login")
+        page = setup_playwright
+        page.goto("https://www.saucedemo.com/")
 
-    def test_login_incorect(self):
-        print ("test_login_incorrect")
+
